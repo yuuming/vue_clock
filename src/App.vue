f61b6f59d45ed61b046de36db08ebb48a568d2ff
Yuumi <yuuming.n@gmail.com>
2019-01-04 21:12:40 +0900

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,72 @@
 <script>
 </script>
 
-<style>
+<style scoped>
+.container {
+  background-color: #3a4a5e;
+  padding: 2%;
+}
+ 
+ 
+.date {
+  text-align: right;
+  color: #fff;
+  font-family: 'Teko', sans-serif;
+  font-size: 4rem;
+  letter-spacing: .1em;
+  margin: .0em 0;
+  line-height: 1;
+}
+ 
+ 
+.time {
+  display: flex;
+}
+ 
+ 
+.time-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1;
+  height: 100px;
+  position: relative;
+  z-index: 1;
+  padding: 0.5em;
+  margin: 3px;
+  color: #fff;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 3rem;
+  line-height: 1;
+  background-color: #48b883;
+  box-sizing: border-box;
+}
+ 
+ 
+.time-item:before {
+  position: absolute;
+  right: 5px;
+  bottom: 1px;
+  z-index: 1;
+  color: #3a4a5e;
+  font-family: 'Teko', sans-serif;
+  font-size: 1.4rem;
+  letter-spacing: .05em;
+}
+ 
+ 
+.hours:before {
+  content: "Hours";
+}
+ 
+ 
+.minutes:before {
+  content: "Minutes";
+}
+ 
+ 
+.seconds:before {
+  content: "Seconds";
+}
 </style>
 
