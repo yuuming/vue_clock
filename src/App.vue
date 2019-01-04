4ea75e5f7c83b22e9aed67602660795d8bdc5d7d
Yuumi <yuuming.n@gmail.com>
2019-01-04 21:11:28 +0900

    Add template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-
+  <div>
+    <div class="container">
+      <p class="date">{{ year }}/{{ month }}/{{ day }}</p>
+      <div class="time">
+        <p class="time-item hours">{{ hours }}</p>
+        <p class="time-item minutes">{{ minutes }}</p>
+        <p class="time-item seconds">{{ seconds }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
-
 </style>
+
