e1ec37123e23f089706b25da0cde1dce6748bbd1
Yuumi <yuuming.n@gmail.com>
2019-01-04 21:11:06 +0900

    Import Clock file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Clock from '@/components/Clock'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Clock',
+      component: Clock
     }
   ]
 })
